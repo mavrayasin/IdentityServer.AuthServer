2afffedd94727204be144b700ac821f9ba993eaa
Yasin ELMAS <yasin.elmas@atilim.edu.tr>
2025-04-15 16:02:36 +0300

    düzenleme

--- IdentityServer.API1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IdentityServer.API1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
